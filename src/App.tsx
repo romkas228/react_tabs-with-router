38eee467db17a45d237833959c0941e4ef03fb3f
Roman Danuliyk <romandanuliyk@gmail.com>
2025-09-07 23:42:00 +0300

    changed NavLink router components to Link

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,44 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 import cn from 'classnames';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
-export const App = () => (
-  <>
-    <nav
-      className="navbar is-light is-fixed-top is-mobile has-shadow"
-      data-cy="Nav"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => {
-              return cn('navbar-item', { 'is-active': isActive });
-            }}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/tabs"
-            className={({ isActive }) => {
-              return cn('navbar-item', { 'is-active': isActive });
-            }}
-          >
-            Tabs
-          </NavLink>
+export const App = () => {
+  const currLocation = useLocation();
+
+  return (
+    <>
+      <nav
+        className="navbar is-light is-fixed-top is-mobile has-shadow"
+        data-cy="Nav"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <Link
+              to="/"
+              className={cn('navbar-item', {
+                'is-active': currLocation.pathname === '/',
+              })}
+            >
+              Home
+            </Link>
+            <Link
+              to="/tabs"
+              className={cn('navbar-item', {
+                'is-active': currLocation.pathname === '/tabs',
+              })}
+            >
+              Tabs
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <div className="section">
+        <div className="container">
+          <Outlet />
         </div>
       </div>
-    </nav>
-
-    <div className="section">
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
